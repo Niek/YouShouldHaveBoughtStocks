5813ee7550215fdb4e1968f6d9b70408f1dd0134
Niek van der Maas <mail@niekvandermaas.nl>
2020-01-30 13:55:46 +0100

    Add update script

---
+++ update-data.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+tickers=$(grep "ticker: " content/_index.md | cut -d: -f3)
+
+for t in $tickers; do
+  price=$(curl -s https://duckduckgo.com/js/spice/stocks/$t | grep -o '"Last":[0-9.]*' | cut -d: -f2)
+  line=$(($(grep -n $t content/_index.md |  cut -d: -f1) + 3))
+  echo "Ticker ${t} is currently \$${price}"
+  sed -i '' "${line}s/.*/    currentprice: ${price}/" content/_index.md
+done
