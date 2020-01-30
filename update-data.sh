735ec49012e4a32fafbacb474531424d94c9954e
Niek van der Maas <mail@niekvandermaas.nl>
2020-01-30 15:37:43 +0100

    Make script work on Linux

--- update-data.sh
+++ update-data.sh
@@ -6,5 +6,9 @@ for t in $tickers; do
   price=$(curl -s https://duckduckgo.com/js/spice/stocks/$t | grep -o '"Last":[0-9.]*' | cut -d: -f2)
   line=$(($(grep -n $t content/_index.md |  cut -d: -f1) + 3))
   echo "Ticker ${t} is currently \$${price}"
-  sed -i '' "${line}s/.*/    currentprice: ${price}/" content/_index.md
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "${line}s/.*/    currentprice: ${price}/" content/_index.md
+  else
+    sed -i "${line}s/.*/    currentprice: ${price}/" content/_index.md
+  fi
 done
