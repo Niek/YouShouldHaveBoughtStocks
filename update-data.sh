b423afb22f528b68c1ccb802a7a13f6403db4fe8
Niek van der Maas <mail@niekvandermaas.nl>
2020-09-02 16:17:18 +0200

    Fix for updating prices

--- update-data.sh
+++ update-data.sh
@@ -4,7 +4,7 @@ tickers=$(grep "ticker: " content/_index.md | cut -d: -f3)
 
 for t in $tickers; do
   price=$(curl -s https://duckduckgo.com/js/spice/stocks/$t | grep -o '"Last":[0-9.]*' | cut -d: -f2)
-  line=$(($(grep -n $t content/_index.md |  cut -d: -f1) + 3))
+  line=$(($(grep -n :${t} content/_index.md |  cut -d: -f1) + 3))
   echo "Ticker ${t} is currently \$${price}"
   if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i '' "${line}s/.*/    currentprice: ${price}/" content/_index.md
