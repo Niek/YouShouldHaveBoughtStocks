3f1f40f86287291f764d4929fcb98c7fd84a85d8
Niek van der Maas <mail@niekvandermaas.nl>
2022-09-14 10:55:37 +0200

    Fix price updater

--- update-data.sh
+++ update-data.sh
@@ -3,7 +3,7 @@
 tickers=$(grep "ticker: " content/_index.md | cut -d: -f3)
 
 for t in $tickers; do
-  price=$(curl -s https://duckduckgo.com/js/spice/stocks/$t | grep -o '"Last":[0-9.]*' | cut -d: -f2)
+  price=$(curl -s "https://query1.finance.yahoo.com/v7/finance/quote?symbols=${t}" | jq -r '.quoteResponse.result[0].regularMarketPrice')
   line=$(($(grep -n :${t} content/_index.md |  cut -d: -f1) + 3))
   echo "Ticker ${t} is currently \$${price}"
   if [[ "$OSTYPE" == "darwin"* ]]; then
