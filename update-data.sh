da925ef423b1d225fb5c97236d1de9668633a980
Niek van der Maas <mail@niekvandermaas.nl>
2023-04-20 11:08:27 +0200

    Use Google for stock price

--- update-data.sh
+++ update-data.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-tickers=$(grep "ticker: " content/_index.md | cut -d: -f3)
+tickers=$(grep "ticker: " content/_index.md | cut -d" " -f6)
 
 for t in $tickers; do
-  price=$(curl -s "https://query1.finance.yahoo.com/v7/finance/quote?symbols=${t}" | jq -r '.quoteResponse.result[0].regularMarketPrice')
+  google_t=$(echo "$t" | awk -F: '{ print $2 ":" $1 }')
+  price=$(curl -s "https://www.google.com/finance/quote/${google_t}" -L | grep -Eo 'data-last-price="([^"]+)' | cut -d '"' -f 2)
   if [ "$price" == "null" ]; then echo "Got wrong price for ticker ${t}, exiting..."; exit 1; fi
-  line=$(($(grep -n ":${t}" content/_index.md |  cut -d: -f1) + 3))
+  line=$(($(grep -n "${t}" content/_index.md |  cut -d: -f1) + 3))
   echo "Ticker ${t} is currently \$${price}"
   if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i '' "${line}s/.*/    currentprice: ${price}/" content/_index.md
